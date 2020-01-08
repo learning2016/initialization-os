b30f14c5d2b10444b5a036f36ce2d6f0e542330f
learning2016 <1330107395@qq.com>
2020-01-08 11:46:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ systemctl disable firewalld.service
 yum -y install iptables-services net-tools
 systemctl enable iptables.service
 yum -y install wget
+yum -y install vim
 mkdir /script
 cd /script
 wget https://raw.githubusercontent.com/learning2016/initialization-os/master/iptables.sh?token=AEMZOTFYEGPYSOJTS75DVL26CVHXI
